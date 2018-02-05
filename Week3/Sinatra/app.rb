1f4c3fde811af4e5ff6f24ba3e4ead50108c5fc3
kotpsot <zygmunt.justyna@gmail.com>
2018-02-05 15:51:27 +0000

    update sinatra app

--- Week3/Sinatra/app.rb
+++ Week3/Sinatra/app.rb
@@ -13,7 +13,7 @@ get '/' do
 end
 
 get '/cat' do
-  "<div>
-  <img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'>
- </div>"
+  "<div style='border: 3px dashed red'>
+     <img src='http://bit.ly/1eze8aE'>
+   </div>"
 end
